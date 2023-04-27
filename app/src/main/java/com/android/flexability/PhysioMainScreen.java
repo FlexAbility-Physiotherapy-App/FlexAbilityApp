bf8115e7441ad0b296196fd5387008c8ea74ca00
Constantinos Vakratsas <c.vakratsas@gmail.com>
2023-04-27 17:43:40 +0300

    Almost finished the UI of the physio main screen

---
+++ app/src/main/java/com/android/flexability/PhysioMainScreen.java
@@ -0,0 +1,14 @@
+package com.android.flexability;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhysioMainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_physio_main_screen);
+    }
+}
