fd4dfe2ffa9212835ee1e7484d2e1d248d26caa3
Stavroula Tsoni <stavroula026@gmail.com>
2023-05-05 02:18:55 +0300

    Finished half static version of Doctor Appointments Screen

---
+++ app/src/main/java/com/android/flexability/DoctorAppointmentsScreen.java
@@ -0,0 +1,14 @@
+package com.android.flexability;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DoctorAppointmentsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_appointmets_screen);
+    }
+}
