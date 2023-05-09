1c90a48aef6566145564daa442394f2f63bba396
Nikolas Papadopoulos <53314070+NikolasPpd@users.noreply.github.com>
2023-05-09 23:11:22 +0300

    Display total number of amenities next to title

--- app/src/main/java/com/android/flexability/PhysioMainScreen.java
+++ app/src/main/java/com/android/flexability/PhysioMainScreen.java
@@ -3,6 +3,7 @@ package com.android.flexability;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PhysioMainScreen extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class PhysioMainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_physio_main_screen);
+
+        // Get total number of amenities and update the title accordingly
+        int totalAmenities = 5; //TODO: Replace with actual number of amenities retrieved from database
+        String amenitiesString = getResources().getString(R.string.amenities, totalAmenities);
+        TextView amenitiesTitle = findViewById(R.id.txtViewAmenities);
+        amenitiesTitle.setText(amenitiesString);
     }
 }
