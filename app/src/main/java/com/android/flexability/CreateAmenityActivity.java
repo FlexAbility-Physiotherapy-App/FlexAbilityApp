3ecbf2a13f3741f87dcbd89ef96100e1191d1f77
Nikolas Papadopoulos <53314070+NikolasPpd@users.noreply.github.com>
2023-05-10 18:22:36 +0300

    Create "New Amenity" screen UI

---
+++ app/src/main/java/com/android/flexability/CreateAmenityActivity.java
@@ -0,0 +1,14 @@
+package com.android.flexability;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAmenityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_amenity);
+    }
+}
