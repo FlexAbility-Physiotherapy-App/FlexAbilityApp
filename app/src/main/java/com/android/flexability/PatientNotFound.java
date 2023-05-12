a32b6b8799c8e4e8acb1cec0b7b9039943bbfd73
StergiosKo <stergiosk202@gmail.com>
2023-05-12 17:58:30 +0300

    Προσθήκη Σελίδας Patient not found
    Πρόσθεσα την σελίδα στην οποία οδηγείται ο γιατρός αν δώσει λάθος ΑΜΚΑ κατα την εύρεση ασθενή

---
+++ app/src/main/java/com/android/flexability/PatientNotFound.java
@@ -0,0 +1,14 @@
+package com.android.flexability;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientNotFound extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_not_found);
+    }
+}
