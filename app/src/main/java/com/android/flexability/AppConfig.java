55f7aadcc22b94a55d67406f1502a743c8744aa8
Nikolas Papadopoulos <53314070+NikolasPpd@users.noreply.github.com>
2023-05-12 15:55:11 +0300

    Add R2 API endpoints and modify network settings

---
+++ app/src/main/java/com/android/flexability/AppConfig.java
@@ -0,0 +1,9 @@
+package com.android.flexability;
+
+public class AppConfig {
+    // Backend server API endpoint
+    public static final String BACKEND_SERVER_IP = "http://192.168.100.45/flexability/";
+    public static final String API_CREATE_PROVISION = "/createProvision.php";
+    public static final String API_GET_PROVISION_COUNT = "/getProvisionsCount.php";
+    public static final String API_GET_PROVISIONS = "/getProvisions.php";
+}
