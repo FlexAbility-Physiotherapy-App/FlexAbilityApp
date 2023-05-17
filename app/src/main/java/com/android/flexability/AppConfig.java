64a7f6019de43b9986fbdb576b665739db73fef9
Constantinos Vakratsas <c.vakratsas@gmail.com>
2023-05-17 16:14:54 +0300

    Added code for the completion of an appointment

--- app/src/main/java/com/android/flexability/AppConfig.java
+++ app/src/main/java/com/android/flexability/AppConfig.java
@@ -2,10 +2,12 @@ package com.android.flexability;
 
 public class AppConfig {
     // Backend server API endpoint
-    public static final String BACKEND_SERVER_IP = "http://192.168.000.000/flexability/";
+    public static final String BACKEND_SERVER_IP = "http://192.168.2.4/flexability/";
     public static final String API_CREATE_PROVISION = "/createProvision.php";
     public static final String API_GET_PROVISION_COUNT = "/getProvisionsCount.php";
     public static final String API_GET_PROVISIONS = "/getProvisions.php";
     public static final String API_GET_APPOINTMENTS = "/getAppointments.php";
     public static final String API_GET_APPOINTMENT_COUNT = "/getAppointmentsCount.php";
+    public static final String API_POST_COMPLETE_APPOINTMENT = "/completeAppointment.php";
+
 }
--- app/src/main/java/com/android/flexability/CurrentAppointmentScreenActivity.java
+++ app/src/main/java/com/android/flexability/CurrentAppointmentScreenActivity.java
@@ -1,14 +1,46 @@
 package com.android.flexability;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CurrentAppointmentScreenActivity extends AppCompatActivity {
+
+    private ConstraintLayout btnBack;
+    private EditText editTextComments;
+    private ConstraintLayout btnComplete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_appointment_screen);
+
+        // Variables to get through the Intent once it is created
+        int physio_id = 1;
+        int patient_id = 3;
+        String timestamp = "2023-05-17 16:05:35";
+
+        btnBack = findViewById(R.id.btnBack);
+        editTextComments = findViewById(R.id.editTextComments);
+        btnComplete = findViewById(R.id.btnComplete);
+
+        btnBack.setOnClickListener(view -> finish());
+
+        btnComplete.setOnClickListener(view -> {
+
+            String comment = editTextComments.getText().toString();
+
+            try {
+                OkHttpHandler okHttpHandler = new OkHttpHandler();
+                okHttpHandler.completeAppointment(physio_id, patient_id, timestamp, comment);
+                Toast.makeText(getApplicationContext(), "Το ραντεβού ολοκληρώθηκε", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            finish();
+        });
     }
 }
