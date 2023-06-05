7eebee9290a915d85abe1238e1d8cbf508ebf8df
Tzelalis <ptzelalis@tringlesoft.com>
2023-06-06 00:16:38 +0300

    Completed R1
    All ΠΣΦ's actions related to the physios are completed.

--- app/src/main/java/com/android/flexability/CustomBasedAdapter.java
+++ app/src/main/java/com/android/flexability/CustomBasedAdapter.java
@@ -9,61 +9,27 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-
 public class CustomBasedAdapter extends BaseAdapter {
 
     Context context;
-    ArrayList<Appointment> appointments;
-    int type; //type 1 creates all 3 colors of cards, while type 2 creates only black cards
+    String nameList[];
+    String amkaList[];
+    String timeList[];
+    int type; //type 1 creates all 3 colors cards, while type 2 creates only black cards
     LayoutInflater inflater;
-    int physioId;
 
-    public CustomBasedAdapter(Context ctx, String response, int type, int id){
+    public CustomBasedAdapter(Context ctx, String nameList[], String amkaList[], String timeList[], int type){
         this.context = ctx;
-        this.appointments = parseJson(response);
+        this.nameList = nameList;
+        this.amkaList = amkaList;
+        this.timeList = timeList;
         this.type = type;
-        this.physioId = id;
         inflater = LayoutInflater.from(ctx);
-    }
-
-    public static ArrayList<Appointment> parseJson(String response) {
-        ArrayList<Appointment> appointments = new ArrayList<>();
-        try{
-            JSONObject jsonObject = new JSONObject(response);
-            JSONArray appointmentsArray = jsonObject.getJSONArray("appointments");
-
-            for (int i = 0; i < appointmentsArray.length(); i++) {
-                JSONObject appointmentObject = appointmentsArray.getJSONObject(i);
-
-                String name = appointmentObject.getString("name");
-                String surname = appointmentObject.getString("surname");
-                String amka = appointmentObject.getString("amka");
-                String timestamp = appointmentObject.getString("timestamp");
-                int patientId = appointmentObject.getInt("patientId");
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                    Appointment appointment = new Appointment(name, surname, amka, timestamp, patientId);
-                    appointments.add(appointment);
-                }
-            }
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        return appointments;
     }
 
     @Override
     public int getCount() {
-        return appointments.size();
+        return amkaList.length;
     }
 
     @Override
@@ -89,9 +55,6 @@ public class CustomBasedAdapter extends BaseAdapter {
                 @Override
                 public void onClick(View v) {
                     Intent intent = new Intent(context, CurrentAppointmentScreenActivity.class);
-                    intent.putExtra("physioId", physioId);
-                    intent.putExtra("patientId", appointments.get(position).getPatientId());
-                    intent.putExtra("timestamp", appointments.get(position).getTimestamp());
                     context.startActivity(intent);
                 }
             });
@@ -105,23 +68,12 @@ public class CustomBasedAdapter extends BaseAdapter {
             row = layoutInflater.inflate(R.layout.activity_custom_list_view_black, parent, false);
         }
 
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            LocalDateTime ldt = LocalDateTime.parse(appointments.get(position).getTimestamp().replace(" ", "T"));
-            OffsetDateTime odt = ldt.atOffset( ZoneOffset.UTC );
-            LocalTime beginTime = odt.toLocalTime();
-            LocalTime endTime = beginTime.plus(1, ChronoUnit.HOURS);
-            String beginTimeString = beginTime.toString();
-            String endTimeString = endTime.toString();
-            String time  = beginTimeString.substring(0,5) + " - " + endTimeString.substring(0,5);
-
-            TextView txtView = (TextView)row.findViewById(R.id.nameText);
-            txtView.setText(appointments.get(position).getName_() + " " + appointments.get(position).getSurname());
-            txtView = (TextView)row.findViewById(R.id.phoneText);
-            txtView.setText(appointments.get(position).getAmka());
-            txtView = (TextView)row.findViewById(R.id.hourText);
-            txtView.setText(time);
-        }
+        TextView txtView = (TextView)row.findViewById(R.id.textView9);
+        txtView.setText(nameList[position]);
+        txtView = (TextView)row.findViewById(R.id.textView11);
+        txtView.setText(amkaList[position]);
+        txtView = (TextView)row.findViewById(R.id.textView7);
+        txtView.setText(timeList[position]);
 
         return row;
     }
--- app/src/main/java/com/android/flexability/PhysioMainScreen.java
+++ app/src/main/java/com/android/flexability/PhysioMainScreen.java
@@ -14,46 +14,46 @@ import java.util.ArrayList;
 
 public class PhysioMainScreen extends AppCompatActivity {
 
-    String json_response;
+    private String json_response;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_physio_main_screen);
 
-        // Code for R2
-        // Get total number of amenities and update the title accordingly
-        updateAmenityCount();
+        // Code for R1
+        // Get total number of physios and update the title accordingly
+        updatePhysioCount();
 
-        Button btnCreateAmenity = findViewById(R.id.btnCreateAmenity);
-        btnCreateAmenity.setOnClickListener(view -> {
-            Intent intent = new Intent(PhysioMainScreen.this, CreateAmenityActivity.class);
+        Button btnCreatePhysio = findViewById(R.id.btnCreatePhysioToChacge);
+        btnCreatePhysio.setOnClickListener(view -> {
+            Intent intent = new Intent(PhysioMainScreen.this, CreatePhysioActivity.class);
             startActivity(intent);
         });
 
-        Button btnShowAllAmenities = findViewById(R.id.btnShowAllAmenities);
-        btnShowAllAmenities.setOnClickListener(view -> {
-            Intent intent = new Intent(PhysioMainScreen.this, DisplayAmenitiesScreenActivity.class);
+        Button btnShowAllPhysios = findViewById(R.id.btnShowAllPhysios);
+        btnShowAllPhysios.setOnClickListener(view -> {
+            Intent intent = new Intent(PhysioMainScreen.this, DisplayPhysiosScreenActivity.class);
             startActivity(intent);
         });
 
-        // Load 2 amenities for the preview list
+        // Load 2 physios for the preview list
         int limit = 2;
-        LinearLayout cardsContainer = findViewById(R.id.previewCardsContainer);
+        LinearLayout cardsContainer = findViewById(R.id.previewPhysioCardContainer);
         LayoutInflater inflater = LayoutInflater.from(this);
 
         try {
-            json_response = new OkHttpHandler().getAmenities(limit);
+            json_response = new OkHttpHandler().getPhysios(limit);
             System.out.println("HTTP Response received successfully");
         }
         catch (Exception e) {
             e.printStackTrace();
         }
 
-        ArrayList<Amenity> previewAmenities = AmenityCardAdapter.parseJson(json_response);
-        for (Amenity amenity: previewAmenities) {
-            View convertView = inflater.inflate(R.layout.amenity_card_layout, cardsContainer, false);
-            AmenityCardAdapter.setCardData(amenity, convertView, this);
+        ArrayList<Physio> previewPhysios = PhysioCardAdapter.parseJson(json_response);
+        for (Physio physio : previewPhysios) {
+            View convertView = inflater.inflate(R.layout.physio_card_layout, cardsContainer, false);
+            PhysioCardAdapter.setCardData(physio, convertView, this);
             cardsContainer.addView(convertView);
         }
     }
@@ -62,20 +62,20 @@ public class PhysioMainScreen extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        // Update the total number of amenities after returning to this screen
-        updateAmenityCount();
+        // Update the total number of physios after returning to this screen
+        updatePhysioCount();
     }
 
-    private void updateAmenityCount() {
-        int totalAmenities = 0;
+    private void updatePhysioCount() {
+        int totalPhysios = 0;
         try {
-            totalAmenities = new OkHttpHandler().getAmenitiesCount();
+            totalPhysios = new OkHttpHandler().getPhysiosCount();
         }
         catch (Exception e) {
             e.printStackTrace();
         }
-        String amenitiesString = getResources().getString(R.string.amenities, totalAmenities);
-        TextView amenitiesTitle = findViewById(R.id.txtViewAmenities);
-        amenitiesTitle.setText(amenitiesString);
+        String physiosString = getResources().getString(R.string.physios, totalPhysios);
+        TextView physiosTitle = findViewById(R.id.textViewPhysios);
+        physiosTitle.setText(physiosString);
     }
 }
